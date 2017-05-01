f5aad7c0a64502b81c1bf3f1333c31c6f7a17a2d
shadownater <jlovas@ualberta.ca>
2017-04-30 22:05:10 -0600

    adding bars

---
+++ Assets/eating.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eating : MonoBehaviour {
+	public float RedBar{ get; set; }
+	public float BlackBar{ get; set; }
+	public float BlueBar{ get; set; }
+
+	public float Health{ get; set; }
+		
+
+	// Use this for initialization
+	void Start () {
+		Health = 1;
+
+		RedBar = 0;
+		BlackBar = 0;
+		BlueBar = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
