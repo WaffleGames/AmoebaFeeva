8b8659cbef09c31d38367191a60b16489bbda388
shadownater <jlovas@ualberta.ca>
2017-05-01 14:52:40 -0600

    working on bars and collisions

---
+++ Assets/enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+	//maybe make this a superclass and then each colour would have an assigned number idk how C# does it though
+	public float value;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
