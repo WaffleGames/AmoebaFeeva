755eb40a24652b4fbac50b98fe1e8173b12ad20c
shadownater <jlovas@ualberta.ca>
2017-05-01 16:22:12 -0600

    figuring out two people working on one scene

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 //code edited from https://noobtuts.com/unity/2d-pong-game
 public class Movement : MonoBehaviour {
-	public float playerSpeed = 30; 
+	public float playerSpeed = 10; 
+	public float playerFriction = 0.5f;
+
 
 	void FixedUpdate () {
 		float v = Input.GetAxisRaw("Vertical");
 		float h = Input.GetAxisRaw("Horizontal");
-
-		GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * playerSpeed;
+		//Vector2 velocity = new Vector2(h, v) * playerSpeed;
+		GetComponent<Rigidbody2D>().AddForce(new Vector2 (playerFriction * h, playerFriction * v));
+		//GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * playerSpeed; //this is what worked before
 	}
 }
