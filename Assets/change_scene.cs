222aad337c1e8b976d927437458de8713d682be4
shadownater <jlovas@ualberta.ca>
2017-04-30 19:22:16 -0600

    ui for project

---
+++ Assets/change_scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_scene : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void changeScene (int sceneNum) {
+		SceneManager.LoadScene (sceneNum);
+	}
+}
