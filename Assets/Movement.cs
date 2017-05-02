9769c37ddc53b05a8e460e7a8679471a80a9de56
nlovas <nlovas@ualberta.ca>
2017-05-01 22:19:24 -0600

    reappearing along other boundary almost works.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,6 +7,12 @@ public class Movement : MonoBehaviour {
 	public float playerFriction = 0.5f;
 	public Vector2 curVelocity;
 	private SpriteRenderer mySpriteRenderer;
+	public Vector3 screenPos;
+	public float amPosX;
+	public float amPosY;
+	private float xBound = Screen.width - 15;
+	private float yBound = Screen.height - 15;
+
 
 	private void Awake(){
 		// get a reference to the SpriteRenderer component on this gameObject
@@ -17,7 +23,7 @@ public class Movement : MonoBehaviour {
 		float v = Input.GetAxisRaw("Vertical");
 		float h = Input.GetAxisRaw("Horizontal");
 		curVelocity = GetComponent<Rigidbody2D> ().velocity;
-		print (curVelocity);
+
 		if (h > 0) { 
 			mySpriteRenderer.flipX = true;
 		}else {
@@ -27,6 +33,28 @@ public class Movement : MonoBehaviour {
 		}
 		//Vector2 velocity = new Vector2(h, v) * playerSpeed;
 		GetComponent<Rigidbody2D>().AddForce(new Vector2 (playerFriction * h, playerFriction * v));
-		//GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * playerSpeed; //this is what worked before
+
+		screenPos = Camera.main.WorldToScreenPoint(transform.position);
+		amPosX = screenPos.x;
+		amPosY = screenPos.y;
+		print (screenPos);
+		//when the amoeba exits boundary, comes back through opposite side
+		//left side
+		if (amPosX < 0){ //too far on the left, so appear on the right
+			//Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, amPosY, screenPos.z)).x;
+			transform.Translate(xBound*Time.deltaTime,0,0,Space.World);
+		} else	//Right side
+			if (amPosX > xBound){
+				transform.Translate((-1)*xBound*Time.deltaTime,0,0,Space.World);
+			}
+		//bottom 
+		if (amPosY < 0){
+			transform.Translate (0,yBound*Time.deltaTime,0,Space.World);
+		}else	//top
+			if (amPosY > yBound){
+				transform.Translate (0,(-1)*yBound*Time.deltaTime,0,Space.World);
+			}
+
+
 	}
 }
