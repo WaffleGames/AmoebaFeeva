f3bf93d44ea545c299c2fd47dadb267b0edc74e0
nlovas <nlovas@ualberta.ca>
2017-05-04 15:31:20 -0600

    not entirely happy with movement but its good enough to continue

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 //code edited from https://noobtuts.com/unity/2d-pong-game
 public class Movement : MonoBehaviour {
-	public float playerSpeed = 20; 
-	public float playerFriction = 0.5f;
+	public float playerSpeed = 5000; 
+	public float playerFriction = .2f;
 	public Vector2 curVelocity;
 	private SpriteRenderer mySpriteRenderer;
 	public Vector3 screenPos;
@@ -26,7 +26,7 @@ public class Movement : MonoBehaviour {
 	void FixedUpdate () {
 		float v = Input.GetAxisRaw("Vertical");
 		float h = Input.GetAxisRaw("Horizontal");
-		curVelocity = GetComponent<Rigidbody2D> ().velocity;
+
 
 		if (h > 0) { 
 			mySpriteRenderer.flipX = true;
@@ -44,24 +44,41 @@ public class Movement : MonoBehaviour {
 		}
 
 
-		//GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * playerSpeed;
 
-		//if(v ==0 && h == 0){
+		//GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * playerSpeed * v);
+		//GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * playerSpeed * h);
+		//GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * playerSpeed;
+		curVelocity = GetComponent<Rigidbody2D> ().velocity;
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2(playerSpeed*h,playerSpeed*v));
+
+
+		/*if (h == 0) {
+			Vector2 temp = new Vector2 ((-1) * playerFriction * curVelocity.x, 0);
+			GetComponent<Rigidbody2D> ().AddForce (temp);
+			curVelocity = GetComponent<Rigidbody2D> ().velocity;
 			roundedVelX = Mathf.Round (curVelocity.x);
+			if (roundedVelX == 0) {
+				GetComponent<Rigidbody2D> ().velocity = (new Vector2 (0,curVelocity.y));
+				curVelocity = GetComponent<Rigidbody2D> ().velocity;
+			}
+		}
+		if(v == 0){
+			Vector2 temp = new Vector2 (0, (-1) * playerFriction * curVelocity.y);
+			GetComponent<Rigidbody2D> ().AddForce (temp);
+			curVelocity = GetComponent<Rigidbody2D> ().velocity;
 			roundedVelY = Mathf.Round (curVelocity.y);
-				if ( roundedVelX==0 || roundedVelY ==0) {
-					if (roundedVelX==0) {
-						GetComponent<Rigidbody2D>().AddForce(new Vector2 (0, playerFriction * v));
-				}
-					if (roundedVelY==0) {
-						GetComponent<Rigidbody2D>().AddForce(new Vector2 (playerFriction * h, 0));
-				}
-			}else	GetComponent<Rigidbody2D>().AddForce(new Vector2 (playerFriction * h, playerFriction * v));
-		//}
-		screenPos = Camera.main.WorldToScreenPoint(transform.position);
-		amPosX = screenPos.x;
-		amPosY = screenPos.y;
-		//print (transform.position);
+			if (roundedVelY == 0) {
+				GetComponent<Rigidbody2D> ().velocity =  (new Vector2 (curVelocity.x, 0));
+			}
+		}
+*/
+
+	
+			
+
+		 
+
+
 
 
 
